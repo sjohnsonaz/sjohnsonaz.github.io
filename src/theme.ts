901592f21053229abfa0efcc0a614f43afe9b206
Sean Johnson <sean.johnson.arizona@gmail.com>
2026-02-08 13:10:29 -0700

    Created theme.

--- src/theme.ts
+++ src/theme.ts
@@ -3,8 +3,37 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   cssVariables: true,
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#000030",
+    },
+    secondary: {
+      main: "rgb(229, 229, 234)",
+    },
+  },
+  shape: {
+    borderRadius: 4,
+  },
   typography: {
     fontFamily: "var(--font-roboto)",
+    h1: {
+      fontSize: 56,
+      fontWeight: 400,
+    },
+    h2: {
+      fontSize: 40,
+    },
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        sx: {
+          borderRadius: "1000px",
+          textTransform: "none",
+        },
+      },
+    },
   },
 });
 
