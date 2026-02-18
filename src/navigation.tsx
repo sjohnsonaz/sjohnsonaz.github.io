d76e284815f37faf9cda4d43516a1eda48b61fae
Sean Johnson <sean.johnson.arizona@gmail.com>
2026-02-18 09:14:06 -0700

    Created icons.

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,11 +1,17 @@
-import { Build, Home, Info, Lightbulb } from "@mui/icons-material";
+import {
+  AutoStories,
+  Home,
+  School,
+  Terminal,
+  TextSnippet,
+} from "@mui/icons-material";
 
 export const navigation = [
   { title: "Sean Johnson", href: "/", icon: <Home /> },
-  { title: "Resume", href: "/resume" },
-  { title: "Masters", href: "/masters" },
-  { title: "Projects", href: "/projects" },
-  { title: "Blog", href: "/blog" },
+  { title: "Resume", href: "/resume", icon: <TextSnippet /> },
+  { title: "Masters", href: "/masters", icon: <School /> },
+  { title: "Projects", href: "/projects", icon: <Terminal /> },
+  { title: "Blog", href: "/blog", icon: <AutoStories /> },
 ];
 
 export const breadcrumb = (part: string) => {
